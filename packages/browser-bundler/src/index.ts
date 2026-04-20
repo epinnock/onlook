035e14c2bad3ee9b84bc6b4c24bfd2bc14b1d670
Ejiro Pinnock <epinnock@gmail.com>
2026-04-20 04:22:56 -0500

    Add esbuild wasm path resolver

--- packages/browser-bundler/src/index.ts
+++ packages/browser-bundler/src/index.ts
@@ -1,1 +1,10 @@
-export {};
+export {
+    ESBUILD_WASM_PATH_GLOBAL_KEY,
+    getEsbuildWasmPath,
+    resolveEsbuildWasmPath,
+    setEsbuildWasmPath,
+    type EsbuildWasmPathLike,
+    type EsbuildWasmPathScope,
+    type ResolveEsbuildWasmPathOptions,
+    type SetEsbuildWasmPathOptions,
+} from './esbuild-path';
